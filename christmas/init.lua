8e6cf919b29a6aab3eb517bca17155756b660f0c
Tucker Bamberg <mootpoint23@gmail.com>
2016-12-05 22:10:30 -0500

    Add license

--- christmas/init.lua
+++ christmas/init.lua
@@ -1,8 +1,20 @@
---christmas
+--christmas mod for minetest
 
--- part of the holiday modpack written by Tucker Bamberg AKA 'mootpoint' and John Cole AKA 'Foz'
-
--- liscened under lgpl v3. File to follow
+--[[
+Copyright (C) 2016 Joseph 'Tucker' Bamberg
+leave room for Foz to copyright if he wants
+This file is part of christmas.
+christmas is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+Stats is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with christmas.  If not, see <http://www.gnu.org/licenses/>.
+]]
 
 -- far from a complete mod use at your own risk
 

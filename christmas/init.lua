8e067200c49216584afb38aea92e39224aaa836d
tucker <mootpoint23@gmail.com>
2016-12-09 13:00:31 -0500

    Add Driedel

--- christmas/init.lua
+++ christmas/init.lua
@@ -126,6 +126,143 @@ minetest.register_node('christmas:present_small_purple', {
 	groups = {oddly_breakable_by_hand = 3,},
 	
 })
+
+local register_driedel = function(name, def, color)
+	def.tiles = {
+		'christmas_driedel_'..color..'_tb.png',
+		'christmas_driedel_'..color..'_tb.png',
+		'christmas_driedel_'..color..'_side_a.png',
+		'christmas_driedel_'..color..'_side_b.png',
+		'christmas_driedel_'..color..'_side_c.png',
+		'christmas_driedel_'..color..'_side_d.png',
+	}
+	def.drawtype = 'nodebox'
+	def.is_ground_content = true
+	def.paramtype = 'light'
+	def.node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, -0.5, -0.0625, 0.0625, 0.5, 0.0625}, -- NodeBox1
+			{-0.375, 0.0625, -0.375, 0.375, 0.1875, 0.375}, -- NodeBox2
+			{-0.3125, -0.125, -0.3125, 0.3125, 0.0625, 0.3125}, -- NodeBox3
+			{-0.25, -0.25, -0.25, 0.25, -0.125, 0.25}, -- NodeBox4
+			{-0.1875, -0.3125, -0.1875, 0.1875, -0.25, 0.1875}, -- NodeBox5
+			{-0.125, -0.375, -0.125, 0.125, -0.3125, 0.125}, -- NodeBox6
+		}
+	}
+	def.groups = {
+			snappy=3
+		}
+	def.inventory_image = 'christmas_driedel_'..color..'.png',
+	minetest.register_node(name, def)
+	
+end
+
+
+minetest.register_node("christmas:driedel_wood", {
+	tiles = {
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, -0.5, -0.0625, 0.0625, 0.5, 0.0625}, -- NodeBox1
+			{-0.375, 0.0625, -0.375, 0.375, 0.1875, 0.375}, -- NodeBox2
+			{-0.3125, -0.125, -0.3125, 0.3125, 0.0625, 0.3125}, -- NodeBox3
+			{-0.25, -0.25, -0.25, 0.25, -0.125, 0.25}, -- NodeBox4
+			{-0.1875, -0.3125, -0.1875, 0.1875, -0.25, 0.1875}, -- NodeBox5
+			{-0.125, -0.375, -0.125, 0.125, -0.3125, 0.125}, -- NodeBox6
+		}
+	}
+})
+
+-- Blue Driedel
+register_driedel('christmas:driedel_blue', {
+	description = 'Blue Driedel',
+}, 'blue')
+
+-- Gold Driedel
+register_driedel('christmas:driedel_gold', {
+	description = 'Gold Driedel',
+}, 'gold')
+
+
+-- Red Driedel
+register_driedel('christmas:driedel_red', {
+	description = 'Red Driedel',
+}, 'red')
+
+
+-- Gray Driedel
+register_driedel('christmas:driedel_gray', {
+	description = 'Gray Driedel',
+}, 'gray')
+
+
+-- Green Driedel
+register_driedel('christmas:driedel_green', {
+	description = 'Green Driedel',
+}, 'green')
+
+
+-- Yellow Driedel
+register_driedel('christmas:driedel_yellow', {
+	description = 'Yellow Driedel',
+}, 'yellow')
+
+-- Craft Recipes for driedel
+
+minetest.register_craft({
+	output = 'christmas:driedel_wood',
+	recipe = {
+		{'default:stick',},
+		{'default:wood',},
+	}
+})
+
+minetest.register_craft({
+	output = 'christmas:driedel_blue',
+	type = 'shapeless',
+	recipe = {'christmas:driedel_wood', 'dye:blue',}
+})
+minetest.register_craft({
+	output = 'christmas:driedel_gold',
+	type = 'shapeless',
+	recipe = {'christmas:driedel_wood', 'dye:green',}
+})
+
+minetest.register_craft({
+	output = 'christmas:driedel_gray',
+	type = 'shapeless',
+	recipe = {'christmas:driedel_wood', 'dye:grey',},
+})
+
+minetest.register_craft({
+	output = 'christmas:driedel_green',
+	type = 'shapeless',
+	recipe = {'christmas:driedel_wood', 'dye:dark_green',},
+})
+
+minetest.register_craft({
+	output = 'christmas:driedel_red',
+	type = 'shapeless',
+	recipe = {'christmas:driedel_wood', 'dye:red',}
+})
+
+minetest.register_craft({
+	output = 'christmas:driedel_yellow',
+	type = 'shapeless',
+	recipe = {'christmas:driedel_wood', 'dye:yellow',}
+})
+-- Snowman 
+
 minetest.register_node('christmas:snowman_head', {
 	description = 'Snowman Head',
 	drawtype = 'nodebox',
